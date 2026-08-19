73fe33fe291847605983bc85167d7a465dc7edb6
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2026-08-19 16:46:33 +0900

    style: group rollout type imports

--- apps/server/src/vector-acceleration.ts
+++ apps/server/src/vector-acceleration.ts
@@ -1,5 +1,4 @@
 import type { VectorRolloutPolicy } from './projection-serving.js';
-
 import type { PgvectorHnswOptions } from '@kotowari/adapter-postgres';
 
 const DEFAULT_EMBEDDING_DIMENSIONS = 8;
